b4cfd58f88279742cdc3246bc112c3fc18d50eff
Martín García <140340907+mitgar14@users.noreply.github.com>
2026-03-12 22:11:06 -0500

    feat(ble): wire BlePanel, TouchToggle and useBle hook into App

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,28 @@ import Scene from './components/Scene'
 import StartScreen from './components/overlay/StartScreen'
 import Overlay from './components/overlay/Overlay'
 import TouchControls from './components/overlay/TouchControls'
+import BlePanel from './components/overlay/BlePanel'
+import TouchToggle from './components/overlay/TouchToggle'
 import { useKeyboardGestures } from './hooks/useKeyboardGestures'
 import { useAudio } from './hooks/useAudio'
+import { useBle } from './hooks/useBle'
 
 export default function App() {
   useKeyboardGestures()
   const audioRef = useAudio()
+  const ble = useBle()
 
   return (
     <>
       <Scene audioRef={audioRef} />
       <Overlay />
       <TouchControls />
+      <BlePanel
+        onStartScan={ble.startScan}
+        onStopScan={ble.stopScan}
+        onConnect={ble.connect}
+      />
+      <TouchToggle />
       <StartScreen />
     </>
   )
